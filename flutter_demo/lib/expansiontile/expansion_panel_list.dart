c54f2aa3bb28c3514e35d798a42e8db1391891fd
张小泉 <zhangxiaoquandemo@gmail.com>
2019-05-19 00:33:57 +0800

    expansion panel list

--- flutter_demo/lib/expansiontile/expansion_panel_list.dart
+++ flutter_demo/lib/expansiontile/expansion_panel_list.dart
@@ -1,8 +1,87 @@
 import 'package:flutter/material.dart';
 
-class expansion_panel_list extends StatelessWidget {
+class expansion_panel_list extends StatefulWidget {
+  @override
+  _expansion_panel_listState createState() => _expansion_panel_listState();
+}
+
+class _expansion_panel_listState extends State<expansion_panel_list> {
+  
+   List<int> indexs;
+   List<expansion_state_bean> bean;
+
+
+  _expansion_panel_listState(){
+    indexs = List();
+    bean = List();
+	for (int i = 0;i<=10;i++) {
+	  	indexs.add(i);
+	  	bean.add(expansion_state_bean(false, indexs));
+	}
+  }
+  
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+	  title: 'expansion panel list',
+	  theme: ThemeData(primarySwatch: Colors.orange),
+	  home: Scaffold(
+		appBar: AppBar(
+		  title: Text('expansion panel list'),
+		  centerTitle: true,
+		),
+		body:SingleChildScrollView(
+		  child: ExpansionPanelList(
+			expansionCallback: (index,bol){
+			  setState(() {
+				bean[index].isOpen = !bol;
+			  });
+			  
+			//  _chang_state(index,bol);
+			},
+			children: indexs.map((index){
+			  return ExpansionPanel(
+				headerBuilder: (context,isExpanded){
+				  return ListTile(
+					title: Text('This is No. $index'),
+				  );
+				},
+				body: ListTile(
+				  title: Text('expansion panel list $index'),
+				),
+				isExpanded: bean[index].isOpen
+			  );
+			}).toList(),
+		  ),
+		),
+	  ),
+	);
   }
+
+   // ignore: non_constant_identifier_names
+   _chang_state(int index,isExpanded){
+   		setState(() {
+   		  bean.forEach((item){
+   		    if(item.index == index){
+   		      item.isOpen = !isExpanded;
+			}
+		  });
+   		});
+  }
+  
 }
+
+
+class expansion_state_bean {
+  var isOpen;
+  var index;
+
+  expansion_state_bean(this.isOpen, this.index);
+  
+  
+  
+ 
+  
+}
+
--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -16,14 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '开始页面',
-      theme: ThemeData(
-        primaryColor: Colors.pink
-      ),//自定义主题
+      theme: ThemeData(primaryColor: Colors.pink), //自定义主题
       home: _Mainpage(),
     );
   }
 }
-
 
 class _Mainpage extends StatelessWidget {
   @override
@@ -33,156 +30,140 @@ class _Mainpage extends StatelessWidget {
         title: Text('Main主页'),
       ),
       body: Container(
-          alignment: Alignment.topLeft,
-          constraints: BoxConstraints.expand(
-            width: double.maxFinite,
-            height: double.maxFinite,
-
-          ),
-          margin: EdgeInsets.all(5.0),
-          decoration: BoxDecoration(
-            color: Colors.orange,
-            border: Border.all(
-                color: Colors.blueAccent,
-                width: 1.0,
-                style: BorderStyle.solid
+        alignment: Alignment.center,
+        constraints: BoxConstraints.expand(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        ),
+        margin: EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          border: Border.all(
+              color: Colors.blueAccent, width: 1.0, style: BorderStyle.solid),
+        ),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          semanticChildCount: 4,
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                '底部导航栏',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => bottom_navgation_widget(),
+                ));
+              },
             ),
-
-          ),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child:RaisedButton(
-                  child: Text('底部导航栏',
-                    style: TextStyle(
-                        color: Colors.black12,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => bottom_navgation_widget(),
-                    ));
-                  },
-                ) ,
+            RaisedButton(
+              child: Text(
+                '不规则底部导航栏',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
               ),
-              Expanded(
-                  child:RaisedButton(
-                    child: Text('不规则底部导航栏',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => bottom_navgation_custom()));
-                    },
-                  ) ,
-                ),
-              Expanded(
-                  child:RaisedButton(
-                    child: Text('路由动画',
-                      style: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => route_page1()));
-                    },
-                  ) ,
-                ),
-                Expanded(
-                  child:RaisedButton(
-                    child: Text('毛玻璃效果',
-                      style: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-                         Navigator.of(context).push(MaterialPageRoute(builder: (context)=>frosted_glass()));
-                    },
-                  ) ,
-                ),
-              Expanded(
-                  child:RaisedButton(
-                    child: Text('头部导航栏',
-                      style: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> top_nav_widget()));
-                    },
-                  ) ,
-                ),
-                Expanded(
-                  child:RaisedButton(
-                    child: Text('搜索栏',
-                      style: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> search_bar_widget()));
-                    },
-                  ) ,
-                ),
-               Expanded(
-                  child:RaisedButton(
-                    child: Text('流式布局',
-                      style: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    onPressed: (){
-                          Navigator.of(context).push(MaterialPageRoute(builder:(context) => wrap_layout() ));
-                    },
-                  ) ,
-                ),
-              Expanded(
-                child:RaisedButton(
-                  child: Text('expansion tile',
-                    style: TextStyle(
-                        color: Colors.black12,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder:(context) => expandion_tile() ));
-                  },
-                ) ,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => bottom_navgation_custom()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                '路由动画',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
               ),
-              Expanded(
-                child:RaisedButton(
-                  child: Text('expansion panel list',
-                    style: TextStyle(
-                        color: Colors.black12,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder:(context) => expansion_panel_list()));
-                  },
-                ) ,
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => route_page1()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                '毛玻璃效果',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
               ),
-            ],
-          )
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => frosted_glass()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                '头部导航栏',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => top_nav_widget()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                '搜索栏',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => search_bar_widget()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                '流式布局',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => wrap_layout()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                'expansion tile',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => expandion_tile()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                'expansion panel list',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => expansion_panel_list()));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
